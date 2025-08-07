0fb7ca592254d91282735b0d9e93a4ac9eb290ac
Emann-code <emmanueladisa39@gmail.com>
2025-08-07 11:03:45 +0100

    initial commit

---
+++ emann.js
@@ -0,0 +1,15 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Example usage:
+let result = addNumbers(5, 7);
+console.log(result); // Output: 12
+
+const PI = 22/7;
+function calcAreaOfCircle(r){
+ return PI*r*r; 
+}
+
+console.log(calcAreaOfCircle(5));
+console.log("The Area of a circle is: " + calcAreaOfCircle(5) + " meters squared");
