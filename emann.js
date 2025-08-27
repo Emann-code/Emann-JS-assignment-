b47e256e981944a428a88dee08b9d590bcbde1fd
Emann-code <emmanueladisa39@gmail.com>
2025-08-27 13:12:27 +0100

    Initial commit

--- emann.js
+++ emann.js
@@ -1,15 +1,51 @@
-function addNumbers(a, b) {
-  return a + b;
-}
+// function addNumbers(a, b) {
+//   return a + b;
+// }
 
-// Example usage:
-let result = addNumbers(5, 7);
-console.log(result); // Output: 12
+// // Example usage:
+// let result = addNumbers(5, 7);
+// console.log(result); // Output: 12
 
-const PI = 22/7;
-function calcAreaOfCircle(r){
- return PI*r*r; 
-}
+// const PI = 22/7;
+// function calcAreaOfCircle(r){
+//  return PI*r*r; 
+// }
 
-console.log(calcAreaOfCircle(5));
-console.log("The Area of a circle is: " + calcAreaOfCircle(5) + " meters squared");
+// console.log(calcAreaOfCircle(5));
+// console.log("The Area of a circle is: " + calcAreaOfCircle(5) + " meters squared");
+
+// function modulus(num1, num2) {
+//   ans + num1 % num2;
+//   return ans;
+// }
+
+// cosole.log("The Modulus is" + modulus(6,4));
+
+
+// const modulus = (a,b) => a % b
+
+// cosole.log(modulus(4,2));
+// cosole.log(modulus(7,2));
+
+// let x = 10;
+// let y = 20;
+// let z = "30";
+//  result = x + y +z 
+
+// let e = 200 / "apple";
+// console.log(e)
+
+ 
+// function getUniqueVowels (str) {
+//     const vowels = "aeiou" ;
+//     const found = new Set () ;
+
+//     for (let char of str.toLowerCase()) {
+//         if (vowels.includes(char)) {
+//             found.add(char) ;
+//         }
+//     }
+//       return Array.from(found) . join ("") ;
+// }
+// console.log(getUniqueVowels("antimonopolygeographicationalism"))
+
